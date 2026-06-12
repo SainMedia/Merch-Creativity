22a450565dcc850e262aaaea06761e57c18c7e63
SainMedia <mhaytb@gmail.com>
2026-06-13 01:43:36 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='merch-creativity-v1';
+const CACHE_NAME='merch-creativity-supabase-v1';
 const ASSETS=['./','./index.html','./manifest.json','./service-worker.js','./assets/icon.png','./assets/logo_merch.png','./assets/product_blue.png','./assets/product_pink.png','./assets/product_monochrome.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
